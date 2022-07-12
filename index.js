9826280ebd6f0e76123308244e757c51d21c2699
daragmas <daragmas@gmail.com>
2022-07-12 14:28:50 -0400

    commit tree 02936582c7422a40acf5f41be61837d29f1d0d08

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  array.sort((a,b)=> a-b)
+  for(var n = 0 ; n<(array.length/2);n++){
+    for(var j = n+1; j<array.length;j++){
+      if(array[n]+array[j]==target){return true}
+    }
+  }
+
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  O(n) for the array.sort()
+  O(n/2) for n for loop
+  O(n) for j for loop
+
+  O(n^3 / 2) total run time
 */
 
 /* 
   Add your pseudocode here
+  Sort array in numerical order
+  starting with first value, check if any of the remaining values work for target
+    if not, check the next value.
+  if none of the values in the array add to target, return false
 */
 
 /*
